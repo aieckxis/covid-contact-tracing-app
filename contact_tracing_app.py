d3e7a1257daa32274b4d26dfa9954e8ee940c173
aieckxis <aleckxiskate@gmail.com>
2023-07-09 16:11:42 +0800

    pseudocode for contact_tracing_app.py

---
+++ contact_tracing_app.py
@@ -0,0 +1,10 @@
+# Bernabe, Aleckxis Kate V.
+# BSCpE 1-4
+
+# Import CSV to enable reading from and writing to CSV files
+# Initialize ContactTracingApp class
+# Write collected information to a CSV file
+# Write the entry to the CSV file and print added successfully
+# Read entries from CSV file and search for a matching name
+# Check if the name matches
+# Print whether the entry is found or not
