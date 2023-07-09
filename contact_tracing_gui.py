1c7260e727461e84274166df22e22027d1772df7
aieckxis <aleckxiskate@gmail.com>
2023-07-09 17:37:45 +0800

    pseudocode for contact_tracng_gui.py

---
+++ contact_tracing_gui.py
@@ -0,0 +1,9 @@
+# Import tkinter module and assign it as tk, and import ContactTracingApp from contact_tracing_app
+# Create and instance of ContactTracingApp
+# Create labels and entry fields
+# Create buttons
+# Place labels and entry fields on grid
+# Place buttons on grid
+# Create the main window
+# Create and instance of contact tracing GUI
+# Run the application
