06bb013c239eccfd785e83c11cde379649ac1c2b
aieckxis <aleckxiskate@gmail.com>
2023-07-09 21:56:15 +0800

    imported tkinter and ContactTracingApp (Have Error, Comma)

--- contact_tracing_gui.py
+++ contact_tracing_gui.py
@@ -1,5 +1,13 @@
 # Import tkinter module and assign it as tk, and import ContactTracingApp from contact_tracing_app
+import tkinter as tk
+from contact_tracing_app import ContactTracingApp
+
 # Create and instance of ContactTracingApp
+class ContactTracingAppGUI:
+    def __init__(self):
+        self.root = root
+        self.root.title(COVID Contact Tracing App)
+        self.contact_tracing_app = ContactTracingAppGUI()
 # Create labels and entry fields
 # Create buttons
 # Place labels and entry fields on grid
