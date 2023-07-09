03b9b73d92fa73f9688e4eefd5bc0bc22af5e5d2
aieckxis <aleckxiskate@gmail.com>
2023-07-09 16:30:08 +0800

    collected information (IndentationError)

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -2,9 +2,20 @@
 # BSCpE 1-4
 
 # Import CSV to enable reading from and writing to CSV files
+import csv
+
 # Initialize ContactTracingApp class
-# Write collected information to a CSV file
-# Write the entry to the CSV file and print added successfully
+class ContactTracingApp:
+    def __init__(self):
+
+    # Write collected information to a CSV file
+    def add_entry(self):
+        with open("contact_tracing.csv", "a", newline="") as file
+            writer = csv.writer(file)
+
+            # Write the entry to the CSV file and print added successfully
+            writer.writerow(name, phone, date)
+            print("Entry added successfully.")
 # Read entries from CSV file and search for a matching name
 # Check if the name matches
 # Print whether the entry is found or not
